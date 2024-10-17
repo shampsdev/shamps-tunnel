29e877b20669e36cde140384db28a7d42ff8b757
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-17 18:51:41 +0300

    added some final changes

--- tunnel.ts
+++ tunnel.ts
@@ -1,21 +1,25 @@
+#!/usr/bin/env node
+
 import { exec } from 'node:child_process';
 import figlet from 'figlet';
 
 const remoteHost = 'linuxserver.io@tunnel.shamps.dev';
 const remotePort = 2222;
-const localPort = 3000;
 const tunnelPort = 8080;
+
+const localPort = process.argv[2] || 5173;
 
 const run = () => {
   const command = `ssh ${remoteHost} -p ${remotePort} -N -R 0.0.0.0:${tunnelPort}:localhost:${localPort}`;
 
   figlet('ShampsTunnel', function (err, data) {
-    // if (err) {
-    //   console.log('Something went wrong...');
-    //   console.dir(err);
-    //   return;
-    // }
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
     console.log(data);
+    console.log(`\n\nRunning tunnel on localhost:${localPort}`);
   });
 
   exec(command, (error, stdout, stderr) => {
